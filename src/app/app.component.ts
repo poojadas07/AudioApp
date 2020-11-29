1280bf1bc96d74768471dab1cdd2e8cc88749ff3
poojadas07 <poojadas04kv@gmail.com>
2020-11-29 11:20:38 +0530

    Function added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AudioApp';
+  title = 'AudioApp'; 
+
+  audioObj = new Audio();
+
+  files = [
+    {
+      url : './assets/song1.mp3',
+      name : 'Song 1'
+    },
+    {
+      url : './assets/song2.mp3',
+      name : 'Song 2'
+    },
+    {
+      url : './assets/song3.mp3',
+      name : 'Song 3'
+    }
+  ] ;
+
+  openFile(url){
+    
+  }
 
   play(){
-
+    console.log("Clicked Play");
   }
 
   pause(){
-
+    console.log("Clicked Pause");
   }
 
   stop(){
-    
+    console.log("Clicked stop");
   }
 }
