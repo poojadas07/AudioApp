b1c9534c210094557a28b7b44b3e1a1fd177fdc6
poojadas07 <poojadas04kv@gmail.com>
2020-12-06 19:16:30 +0530

    Service added

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { from, Observable } from 'rxjs';
+import { HttpClient , HttpHeaders} from '@angular/common/http'
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -9,12 +8,18 @@ import { map } from 'rxjs/operators';
 export class SearchService {
 
   private clientId: string = '44b6976203c748c8b55f7654246ff550';
-  private artistsUrl: string = 'https://api.spotify.com/v1/search?type=artist&limit=10&client_id='+this.clientId+'&q=';
+  private searchUrl: string;
+  //private artistsUrl: string = 'https://api.spotify.com/v1/search?type=artist&limit=10&client_id='+this.clientId+'&q=';
 
   constructor(private http: HttpClient) { }
 
-  searchArtists(searchTerm: string) {
-    let url = this.artistsUrl + searchTerm;
-    return this.http.get(url).pipe(map((res:any) => res.json()));
-  }
+  searchMusic(searchTerm: string , type='artist'){
+    this.searchUrl = 'https://api.spotify.com/v1/search?query='+ searchTerm+'&offset=0&limit=20&type='+type;
+    return this.http.get(this.searchUrl).pipe(map((res:any) => res.json()));
+}
+
+  // searchArtists(searchTerm: string) {
+  //   let url = this.artistsUrl + searchTerm;
+  //   return this.http.get(url).pipe(map((res:any) => res.json()));
+  // }
 }
