c1da2432a24e0806239b026617b3522e9dbdee82
poojadas07 <poojadas04kv@gmail.com>
2020-12-08 19:22:36 +0530

    Client id added

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
 })
 export class SearchService {
 
-  private clientId: string = '';
+  private clientId: string = '44b6976203c748c8b55f7654246ff550';
   private searchUrl: string;
   //private artistsUrl: string = 'https://api.spotify.com/v1/search?type=artist&limit=10&client_id='+this.clientId+'&q=';
 
