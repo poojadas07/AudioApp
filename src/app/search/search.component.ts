073d02d5fcb38f8b3069185890bef9509bd2b8c4
poojadas07 <poojadas04kv@gmail.com>
2020-12-11 19:20:31 +0530

    ts file added

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { SearchService } from './search.service';
+import { Artist } from '../../Artist';
 
 @Component({
   selector: 'app-search',
@@ -9,17 +10,27 @@ import { SearchService } from './search.service';
 })
 export class SearchComponent implements OnInit {
   inputField: FormControl = new FormControl();
-  searchResults: any[] = [];
+  
+  searchStr: string;
+  searchRes: Artist[];
 
   constructor(private searchService: SearchService) { }
 
   ngOnInit() {
-    this.inputField.valueChanges
-      .subscribe(inputField => this.searchService.searchArtists(inputField)
-      .subscribe(result => {
-          if(result.status === 400) { return;}
-          else { this.searchResults = result.artists.items; }
-      }));
+    // this.inputField.valueChanges
+    //   .subscribe(inputField => this.searchService.searchArtists(inputField)
+    //   .subscribe(result => {
+    //       if(result.status === 400) { return;}
+    //       else { this.searchResults = result.artists.items; }
+    //   }));
+  }
+
+  searchMusic(event: any) { 
+    this.searchStr = event.target.value ;
+    this.searchService.searchMusic(this.searchStr)
+      .subscribe((res:any) => {
+        this.searchRes = res;
+      })
   }
 
 }
